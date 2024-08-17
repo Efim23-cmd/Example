0471a88b2a7b7446081685b8b6483f04f8ab2417
efim <efimtuhvatullin@gmail.com>
2024-08-17 10:50:58 +0300

    feat: add eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,29 @@
+import js from '@eslint/js';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default [
-	{ files: ['**/*.{js,mjs,cjs,ts}'] },
-	{ languageOptions: { globals: globals.browser } },
+	{
+		plugins: {
+			'@typescript-eslint': tseslint.plugin,
+			prettier: eslintPluginPrettier,
+		},
+	},
+	{
+		files: ['src/**/*.{js, ts}'],
+	},
+	{
+		languageOptions: {
+			globals: { ...globals.browser },
+			/* parserOptions: {
+				project: ['tsconfig.json'],
+			}, */
+		},
+	},
+	{
+		ignores: ['node_modules', 'webpack.config.js', 'dist'],
+	},
+	js.configs.recommended,
 	...tseslint.configs.recommended,
 ];
